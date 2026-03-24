8c72eb8b905f83de3fa14c272903bd361c21aefb
andreacano7 <andcanllo@alu.edu.gva.es>
2026-03-24 16:47:11 +0100

    Registro implementado por Desarrollador 2

--- peliculas-v4/src/main/java/com/example/peliculas/entity/Categoria.java
+++ peliculas-v4/src/main/java/com/example/peliculas/entity/Categoria.java
@@ -4,6 +4,7 @@ public class Categoria {
 	 private int idCategoria;
 	    private String nombre;
 
+	    System.out.println("Funcionalidad de registro");
 
 	    
 	    public Categoria(int idCategoria, String nombre) {
