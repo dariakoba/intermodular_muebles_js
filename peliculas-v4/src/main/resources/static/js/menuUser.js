bdfeabd0045006b39db3be5d3c55f04b8422f77d
dariakoba <darkob@alu.edu.gva.es>
2026-04-26 15:34:49 +0200

    todo el front y js bien con back

--- peliculas-v4/src/main/resources/static/js/menuUser.js
+++ peliculas-v4/src/main/resources/static/js/menuUser.js
@@ -1,13 +1,11 @@
 const personIcon = document.getElementById("person-icon");
 const personMenu = document.querySelector(".person-menu");
 
-// Alterna la clase 'show' al hacer clic
 personIcon.addEventListener("click", (e) => {
-  e.stopPropagation(); // evita que se cierre inmediatamente
+  e.stopPropagation(); 
   personMenu.classList.toggle("show");
 });
 
-// Cierra el menú si se hace clic fuera
 document.addEventListener("click", () => {
   personMenu.classList.remove("show");
 });
@@ -15,7 +13,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   const icon = document.getElementById("person-icon");
   const menu = document.getElementById("person-menu");
 
-  // abrir/cerrar menú al hacer click
   icon.addEventListener("click", () => {
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   });
@@ -27,7 +24,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const user = await res.json();
 
-    // usuario logueado
     menu.innerHTML = `
       <span style="padding:10px; display:block;">Hola, ${user.nombre}</span>
       <a href="perfil.html">Ver cuenta</a>
@@ -35,13 +31,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     `;
 
   } catch {
-    // usuario NO logueado
     menu.innerHTML = `
       <a href="login.html">Iniciar sesión</a>
     `;
   }
 
-  // cerrar menú al hacer click fuera
   document.addEventListener("click", (e) => {
     if (!e.target.closest(".person-menu-wrapper")) {
       menu.style.display = "none";
