aa0ad990d7b8996be7f1bf49bd1d4b1fe881726c
dariakoba <darkob@alu.edu.gva.es>
2026-05-23 11:24:11 +0200

    eliminé la carpeta de adn

--- ADN_muebles/src/main/java/com/dna/muebles/dto/CategoriaDetalle.java
+++
@@ -1,9 +0,0 @@
-package com.dna.muebles.dto;
-
-import com.dna.muebles.entity.Categoria;
-
-public record CategoriaDetalle(
-        int id_categoria,
-        String nombre,
-        String estado
-) {}
